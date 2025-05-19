960b318c6e6cbb167611cd4450f73e3bbf09e566
BinarySonata <24103094@usc.edu.ph>
2025-05-19 14:32:11 +0800

    add products page

--- js/modules/header-footer-link.js
+++ js/modules/header-footer-link.js
@@ -1,26 +1,29 @@
 // import components
 import checkUserAccessToken from "../components/checkAccessToken.js";
 
+let shoppingCart = document.querySelector('a#shoppingBagCart');
+console.log(shoppingCart);
+
 document.addEventListener('DOMContentLoaded', ()=>{
     // Get header and footer logo link
     let headerLogoLink = document.getElementById('header-logo-img-link'); // return <a> element
     let footerLogoLink = document.getElementById('footer-logo-img-link'); // return <a> element
-    let shoppingCart = document.getElementById('shopping-bag-cart');
-    console.log(shoppingCart);
 
 
     const userAccess = checkUserAccessToken(); // returns boolean
     let logoLinkTo = undefined;
-    let cartLink;
+    let cartLink = undefined;
     if(userAccess){
         logoLinkTo = '../pages/all-products.html'; // home when there is user logged in
         cartLink = '../pages/cart-and-checkout.html';
+        console.log(cartLink);
     }else{
         logoLinkTo = '../index.html'; // homepage when no user logged in
         cartLink = '../pages/login.html';
+        console.log(cartLink);
     }
 
-    console.log(shoppingCart.href);
+    shoppingCart.href = "";
     headerLogoLink.href = logoLinkTo;
     footerLogoLink.href = logoLinkTo;
 
